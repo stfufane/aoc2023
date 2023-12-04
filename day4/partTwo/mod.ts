8bdfc99ea3e9d52b7e0496964c75ac5bdefb92b5
Stephane Albanese <stephane.albanese@arturia.com>
2023-12-04 19:40:16 +0100

    Optimize day 4 (x450)

--- day4/partTwo/mod.ts
+++ day4/partTwo/mod.ts
@@ -1,3 +1,6 @@
+// For faster access because we calculate the same thing multiple times
+const results_cache = new Map<number, number>();
+
 export function partTwo(cards: Map<number, number>): number {
   return Array.from(cards.keys()).map((card_id: number): number => {
     return getNbWinningCards(cards, card_id);
@@ -9,12 +12,17 @@ export function getNbWinningCards(
   cards: Map<number, number>,
   card_id: number,
 ): number {
+  if (results_cache.has(card_id)) {
+    return results_cache.get(card_id)!;
+  }
+
   const winning_numbers: number = cards.get(card_id)!;
   if (winning_numbers === 0) {
+    results_cache.set(card_id, 1);
     return 1;
   }
 
-  return Array.from(
+  const total = Array.from(
     { length: winning_numbers },
     (_, i) => card_id + i + 1,
   )
@@ -22,4 +30,6 @@ export function getNbWinningCards(
       (acc: number, val: number) => acc + getNbWinningCards(cards, val),
       1,
     );
+  results_cache.set(card_id, total);
+  return total;
 }
