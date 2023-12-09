a5639d876520c2b41be6b1712a540025f18e57bd
Stephane Albanese <albanese.stephane@gmail.com>
2023-12-09 13:58:05 +0100

    Day 9 - More elegant loop

--- day9/partOne/mod.ts
+++ day9/partOne/mod.ts
@@ -2,17 +2,10 @@ import { getDepthNumbers, Report } from "../mod.ts";
 
 export function partOne(reports: Report[]): number {
   return reports.map(getDepthNumbers)
-    .map((depth_numbers) => {
-      depth_numbers.map((numbers, idx) => {
-        if (idx > 0) {
-          numbers.push(
-            numbers[numbers.length - 1] +
-              depth_numbers[idx - 1][depth_numbers[idx - 1].length - 1],
-          );
-        }
-        return numbers;
-      });
-      return depth_numbers.pop()!.pop()!;
-    })
+    .map((depth_numbers) =>
+      depth_numbers
+        .flatMap((numbers) => numbers.slice(-1))
+        .reduce((a, b) => a + b, 0)
+    )
     .reduce((a, b) => a + b, 0);
 }
